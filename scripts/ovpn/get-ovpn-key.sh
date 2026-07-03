e9c0ce9163961655f1d0e40b5743ba1bcd092875
Hein Htet Zaw <h3inhtetzaw346@gmail.com>
2026-07-03 14:23:17 +0630

    feature: added split tunnel and local dns options to ovpn

--- scripts/ovpn/get-ovpn-key.sh
+++ scripts/ovpn/get-ovpn-key.sh
@@ -7,8 +7,25 @@ mkdir -p ${KEY_DIR}
 echo "==> [CONFIG] Checking required variables..."
 : "${KEY_NAME:?Missing KEY_NAME}"
 
+OVPN_SPLIT_TUNNEL="${OVPN_SPLIT_TUNNEL:-false}"
+OVPN_LOCAL_DNS="${OVPN_LOCAL_DNS:-false}"
+
 echo "==> [GENERATE] Generating OVPN client key..."
 sudo bash ${INSTALL_FILE} client add ${KEY_NAME} > /dev/null
+
+KEY_FILE="$HOME/${KEY_NAME}.ovpn"
+
+if [ "$OVPN_SPLIT_TUNNEL" = "true" ]; then
+    echo "OVPN_SPLIT_TUNNEL is set to true, adding split tunnel config to client file"
+    echo "route 10.0.0.0 255.0.0.0 net_gateway" >> "${KEY_FILE}"
+    echo "route 172.16.0.0 255.240.0.0 net_gateway" >> "${KEY_FILE}"
+    echo "route 192.168.0.0 255.255.0.0 net_gateway" >> "${KEY_FILE}"
+fi
+
+if [ "$OVPN_LOCAL_DNS" = "true" ]; then
+    echo "OVPN_LOCAL_DNS is set to true, adding local dns config to client file"
+    echo 'pull-filter ignore "dhcp-option DNS"' >> ${KEY_FILE}
+fi
 
 mv $HOME/${KEY_NAME}.ovpn ${KEY_DIR}
 
