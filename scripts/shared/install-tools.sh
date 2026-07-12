5b0b0fd53973b55e70f90f6fefa0c492a4f0af21
Hein Htet Zaw <maintain1346@gmail.com>
2026-07-12 12:35:12 +0630

    feature: added install tools job to all

---
+++ scripts/shared/install-tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "==> [CONFIG] Checking required variables..."
+: "${INSTALL_TOOLS:?Missing INSTALL_TOOLS}"
+
+if [ "${INSTALL_TOOLS}" = "true" ]; then
+	echo "==> [INSTALL] Installing required tools"
+	sudo apt install vnstat -y
+else
+	echo "==> [EXIT] Skipping install tools"
+	exit 0
+fi
+
+echo "==> [SUCCESS] Tools installed successfully"
+exit 0
