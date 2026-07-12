4a38dd09c8af7afe103c551eda16065037589cee
Hein Htet Zaw <maintain1346@gmail.com>
2026-07-12 13:32:20 +0630

    feature: added apt update into install-tools

--- scripts/shared/install-tools.sh
+++ scripts/shared/install-tools.sh
@@ -5,6 +5,7 @@ echo "==> [CONFIG] Checking required variables..."
 
 if [ "${INSTALL_TOOLS}" = "true" ]; then
 	echo "==> [INSTALL] Installing required tools"
+	sudo apt update
 	sudo apt install vnstat -y
 else
 	echo "==> [EXIT] Skipping install tools"
