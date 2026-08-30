39909cea82a4452f2ec844004f4bddd0a300228d
Hein Htet Zaw <maintain1346@gmail.com>
2026-08-30 22:25:36 +0630

    removed the third port from output

--- tofu/aws-ec2/outputs.tf
+++ tofu/aws-ec2/outputs.tf
@@ -5,8 +5,7 @@ output "instance_ip" {
 output "randomized_ports" {
   value = [
     random_shuffle.vpn_ports.result[0],
-    random_shuffle.vpn_ports.result[1],
-    random_shuffle.vpn_ports.result[2]
+    random_shuffle.vpn_ports.result[1]
   ]
 }
 
