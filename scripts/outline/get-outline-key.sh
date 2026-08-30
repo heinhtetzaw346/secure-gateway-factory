fd4c1dc0817e4828bbef373b57085c6f1e43a6e1
Hein Htet Zaw <maintain1346@gmail.com>
2026-08-30 12:09:41 +0630

    refactor: moved outline key generation to remote side

--- scripts/outline/get-outline-key.sh
+++ scripts/outline/get-outline-key.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-KEY_DIR="keys/outline"
+KEY_DIR="$HOME/keys/outline"
 KEY_FILE="${KEY_DIR}/${KEY_NAME}.json"
+API_FILE="$HOME/outline/outline-api.json"
 
 mkdir -p ${KEY_DIR}
 echo "==> [CONFIG] Checking required variables..."
-: "${API_URL:?Missing API_URL}"
 : "${KEY_NAME:?Missing KEY_NAME}"
 
-echo "==> [GENERATE] Generating and naming access key..."
+if [ -z "${API_URL}" ]; then
+    if [ ! -f "$API_FILE" ]; then
+        echo "[ERROR] API configuration file $API_FILE not found."
+        exit 1
+    fi
+    RAW_API_URL=$(jq -r '.apiUrl' "$API_FILE")
+    API_URL=$(echo "$RAW_API_URL" | sed -E 's|https://[^:]+:|https://127.0.0.1:|')
+else
+    API_URL=$(echo "$API_URL" | sed -E 's|https://[^:]+:|https://127.0.0.1:|')
+fi
+
+echo "==> [GENERATE] Generating and naming access key via localhost..."
 curl -k -s -X POST "$API_URL/access-keys" > $KEY_FILE
 ID=$(jq -r '.id' $KEY_FILE)
 
@@ -22,3 +33,4 @@ echo "==> [SUCCESS] ${KEY_NAME} key generated and saved to ${KEY_FILE}"
 
 KEY=$(jq -r '.accessUrl' $KEY_FILE)
 echo $KEY
+
