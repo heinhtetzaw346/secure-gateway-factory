42dc67116b4bdaf51411b85f69c23943b0616e7c
Hein Htet Zaw <maintain1346@gmail.com>
2026-08-30 20:47:37 +0630

    added uuid suffix to output

--- tofu/do/outputs.tf
+++ tofu/do/outputs.tf
@@ -5,11 +5,10 @@ output "instance_ip" {
 output "randomized_ports" {
   value = [
     random_shuffle.vpn_ports.result[0],
-    random_shuffle.vpn_ports.result[1],
-    random_shuffle.vpn_ports.result[2]
+    random_shuffle.vpn_ports.result[1]
   ]
 }
 
 output "randomized_uuid" {
-  value = random_id.suffix
+  value = random_id.suffix.hex
 }
